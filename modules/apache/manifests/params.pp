5d0bad3bbce1e9827517f23bb21bf7a6d98ef93b
Sp4ceCowb0y <rfiguerasdegea@outlook.com>
2022-05-08 21:09:48 +0200

    vars without case or if

--- modules/apache/manifests/params.pp
+++ modules/apache/manifests/params.pp
@@ -4,12 +4,12 @@ class apache::params {
   $virtualhost_port = '80'
 
 #  case $facts['osfamily'] {
-  if $osfamily == 'Debian' {
+#  if $osfamily == 'Debian' {
         $version = '2.4.*'
         $package_name = 'apache2'
         $provider = 'apt'
         $ports_path = '/etc/apache2/ports.conf'
         $virtualhost_path = '/etc/apache2/sites-available/000-default.conf'
-    }
+#    }
 #    elsif $osfamily == 'windows' {
 }
