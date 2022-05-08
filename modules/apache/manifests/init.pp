1300c861eecdeae9039d569f6931420f2002d34b
Sp4ceCowb0y <rfiguerasdegea@outlook.com>
2022-05-08 21:27:43 +0200

    removed semicolons

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -1,12 +1,12 @@
 class apache (
 
-  $listen_port = $::apache::params::listen_port,
-  $virtualhost_port = $::apache::params::virtualhost_port,
-  $version = $::apache::params::version,
-  $package_name = $::apache::params::package_name,
-  $provider = $::apache::params::provider,
-  $ports_path = $::apache::params::ports_path,
-  $virtualhost_path = $::apache::params::virtualhost_path,
+  $listen_port = $apache::params::listen_port,
+  $virtualhost_port = $apache::params::virtualhost_port,
+  $version = $apache::params::version,
+  $package_name = $apache::params::package_name,
+  $provider = $apache::params::provider,
+  $ports_path = $apache::params::ports_path,
+  $virtualhost_path = $apache::params::virtualhost_path,
 
 )
 {
@@ -38,17 +38,3 @@ class apache (
   }
 
 }
-
-
-# comm
-
-
-
-
-
-
-
-
-
-
-
