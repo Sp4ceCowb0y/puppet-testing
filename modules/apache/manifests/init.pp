77a4b2da8aac35592ec1b7098bb4bbe5ab9e639e
Sp4ceCowb0y <rfiguerasdegea@outlook.com>
2022-05-08 19:33:05 +0200

    puppet changes for hiera params

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -1,12 +1,25 @@
 class apache (
 
-  $status = installed
+  $status = installed,
+  $listen_port = '80',
+  $virtualhost_port = '80'
 
 )
 {
 
   package { 'apache2':
     ensure => $status
+  }
+
+  file { '/etc/apache2/ports.conf':
+    ensure  => 'file',
+    path    => '/etc/apache2/ports.conf',
+    content => template ('apache/ports.conf.erb'),
+  }
+  file { '/etc/apache2/sites-available/000-default.conf':
+    ensure  => 'file',
+    path    => '/etc/apache2/sites-available/000-default.conf',
+    content => template ('apache/000-default.conf.erb'),
   }
 
 }
