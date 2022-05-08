3b8ae3813ef6896ede43424653b3bea2b1382c5f
Sp4ceCowb0y <rfiguerasdegea@outlook.com>
2022-05-08 20:56:35 +0200

    test facter

--- modules/apache/manifests/params.pp
+++ modules/apache/manifests/params.pp
@@ -5,8 +5,8 @@ class apache::params {
 
   case $facts['os']['family'] {
     'Debian': {
-        $version = '2.4.*'
         $package_name = 'apache2'
+        $version = '2.4.*'
         $provider = 'apt'
         $ports_path = '/etc/apache2/ports.conf'
         $virtualhost_path = '/etc/apache2/sites-available/000-default.conf'
@@ -16,6 +16,13 @@ class apache::params {
     }
     'osx': {
 
+    }
+    default: {
+        $package_name = 'apache2'
+        $version = '2.4.*'
+        $provider = 'apt'
+        $ports_path = undef
+        $virtualhost_path = undef
     }
   }
 
