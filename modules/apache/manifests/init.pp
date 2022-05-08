65f359a19de2f38eabf9089382c9ed7358f8f941
Sp4ceCowb0y <rfiguerasdegea@outlook.com>
2022-05-08 20:42:12 +0200

    parametrization by OS

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -1,25 +1,56 @@
 class apache (
 
-  $status = installed,
-  $listen_port = '80',
-  $virtualhost_port = '80'
+  $listen_port = $::apache::params::listen_port,
+  $virtualhost_port = $::apache::params::virtualhost_port,
+  $version = $::apache::params::version,
+  $package_name = $::apache::params::package_name,
+  $provider = $::apache::params::provider,
+  $ports_path = $::apache::params::ports_path,
+  $virtualhost_path = $::apache::params::virtualhost_path
 
 )
 {
 
-  package { 'apache2':
-    ensure => $status
+  package { 'apache':
+    ensure   => $version,
+    package  => $package_name,
+    provider => $provider
   }
 
-  file { '/etc/apache2/ports.conf':
-    ensure  => 'file',
-    path    => '/etc/apache2/ports.conf',
-    content => template ('apache/ports.conf.erb'),
+  service { 'apache':
+    ensure    => running,
+    enable    => true,
+    subscribe => [File['ports.conf'],File['000-default.conf']],
   }
-  file { '/etc/apache2/sites-available/000-default.conf':
+
+  file { 'ports.conf':
     ensure  => 'file',
-    path    => '/etc/apache2/sites-available/000-default.conf',
+    path    => $ports_path,
+    content => template ('apache/ports.conf.erb'),
+    require => Package['apache']
+  }
+
+  file { '000-default.conf':
+    ensure  => 'file',
+    path    => $virtualhost_path,
     content => template ('apache/000-default.conf.erb'),
+    require => Package['apache']
   }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
