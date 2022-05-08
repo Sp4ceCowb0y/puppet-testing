46d4dfd9ac4cc23fc60d67d971f4666c678b7f5b
Sp4ceCowb0y <rfiguerasdegea@outlook.com>
2022-05-08 21:21:08 +0200

    package -> name

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -2,15 +2,14 @@ class apache (
 
   $listen_port = $::apache::params::listen_port,
   $virtualhost_port = $::apache::params::virtualhost_port,
-  $version = $::apache::params::version
+  $version = $::apache::params::version,
 
-) inherits apache::params
-{
+) inherits apache::params {
 
   package { 'apache':
     ensure   => $version,
-    package  => $package_name,
-    provider => $provider
+    name     => $package_name,
+    provider => $provider,
   }
 
   service { 'apache':
@@ -23,14 +22,14 @@ class apache (
     ensure  => 'file',
     path    => $ports_path,
     content => template ('apache/ports.conf.erb'),
-    require => Package['apache']
+    require => Package['apache'],
   }
 
   file { '000-default.conf':
     ensure  => 'file',
     path    => $virtualhost_path,
     content => template ('apache/000-default.conf.erb'),
-    require => Package['apache']
+    require => Package['apache'],
   }
 
 }
