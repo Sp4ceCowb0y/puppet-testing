44ffb906dc5aa83e3aca6506193108cec6b8e167
Sp4ceCowb0y <rfiguerasdegea@outlook.com>
2022-05-08 21:10:55 +0200

    directlyvars

--- modules/apache/manifests/params.pp
+++ modules/apache/manifests/params.pp
@@ -2,14 +2,10 @@ class apache::params {
 
   $listen_port = '80'
   $virtualhost_port = '80'
+  $version = '2.4.*'
+  $package_name = 'apache2'
+  $provider = 'apt'
+  $ports_path = '/etc/apache2/ports.conf'
+  $virtualhost_path = '/etc/apache2/sites-available/000-default.conf'
 
-#  case $facts['osfamily'] {
-#  if $osfamily == 'Debian' {
-        $version = '2.4.*'
-        $package_name = 'apache2'
-        $provider = 'apt'
-        $ports_path = '/etc/apache2/ports.conf'
-        $virtualhost_path = '/etc/apache2/sites-available/000-default.conf'
-#    }
-#    elsif $osfamily == 'windows' {
 }
