a36762c36328e70c4a44b45df8919a8daa6e03be
Sp4ceCowb0y <rfiguerasdegea@outlook.com>
2022-05-08 21:02:34 +0200

    facter params tests

--- modules/apache/manifests/params.pp
+++ modules/apache/manifests/params.pp
@@ -3,10 +3,10 @@ class apache::params {
   $listen_port = '80'
   $virtualhost_port = '80'
 
-  case $facts['os']['family'] {
-    'Debian': {
-        $package_name = 'apache2'
+  case $facts['os']['name'] {
+    'Ubuntu': {
         $version = '2.4.*'
+        $package_name = 'apache2'
         $provider = 'apt'
         $ports_path = '/etc/apache2/ports.conf'
         $virtualhost_path = '/etc/apache2/sites-available/000-default.conf'
