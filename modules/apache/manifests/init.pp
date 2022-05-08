4331b5c9e30e6edec9344c6eda8264c63dafb478
Sp4ceCowb0y <rfiguerasdegea@outlook.com>
2022-05-08 20:58:19 +0200

    params tests

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -2,8 +2,8 @@ class apache (
 
   $listen_port = $::apache::params::listen_port,
   $virtualhost_port = $::apache::params::virtualhost_port,
-  $version = $::apache::params::version,
   $package_name = $::apache::params::package_name,
+  $version = $::apache::params::version,
   $provider = $::apache::params::provider,
   $ports_path = $::apache::params::ports_path,
   $virtualhost_path = $::apache::params::virtualhost_path
