a7b8a4ae02c20e9b55f78a90d27196f30f25d947
Sp4ceCowb0y <rfiguerasdegea@outlook.com>
2022-05-08 21:53:42 +0200

    readded case to params.pp

--- modules/apache/manifests/params.pp
+++ modules/apache/manifests/params.pp
@@ -2,11 +2,25 @@ class apache::params {
 
   $listen_port = '80'
   $virtualhost_port = '80'
-  $version = '2.4.*'
-  $package_name = 'apache2'
-  $provider = 'apt'
-  $service_name = 'apache2'
-  $ports_path = '/etc/apache2/ports.conf'
-  $virtualhost_path = '/etc/apache2/sites-available/000-default.conf'
+
+  case $::osfamily {
+    'Debian': {
+        $version = '2.4.*'
+        $package_name = 'apache2'
+        $provider = 'apt'
+        $service_name = 'apache2'
+        $ports_path = '/etc/apache2/ports.conf'
+        $virtualhost_path = '/etc/apache2/sites-available/000-default.conf'
+    }
+    'windows': {
+
+    }
+    'osx': {
+
+    }
+    default: {
+      # Empty
+    }
+  }
 
 }
