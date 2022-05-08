20e95aa5f1ebe3abbcf7723b1ac8e8140ffecbfd
Sp4ceCowb0y <rfiguerasdegea@outlook.com>
2022-05-08 20:45:54 +0200

    added class

--- modules/apache/manifests/params.pp
+++ modules/apache/manifests/params.pp
@@ -1,18 +1,22 @@
-$listen_port = '80'
-$virtualhost_port = '80'
+class apache::params {
 
-case $::os['family'] {
-  'Debian': {
-      $version = '2.4.*'
-      $package_name = 'apache2'
-      $provider = 'apt'
-      $ports_path = '/etc/apache2/ports.conf'
-      $virtualhost_path = '/etc/apache2/sites-available/000-default.conf'
-  }
-  'windows': {
+  $listen_port = '80'
+  $virtualhost_port = '80'
 
-  }
-  'osx': {
+  case $::os['family'] {
+    'Debian': {
+        $version = '2.4.*'
+        $package_name = 'apache2'
+        $provider = 'apt'
+        $ports_path = '/etc/apache2/ports.conf'
+        $virtualhost_path = '/etc/apache2/sites-available/000-default.conf'
+    }
+    'windows': {
 
+    }
+    'osx': {
+
+    }
   }
+
 }
